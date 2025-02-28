e28bbe9b59cb86ea422315e239f63e283df36ebf
postwo <codo7717@naver.com>
2025-02-28 21:45:20 +0900

    유효성 검사 구현

--- src/main/java/com/example/crash/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/crash/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.crash.exception;
 
 import com.example.crash.model.error.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +18,30 @@ public class GlobalExceptionHandler {
                 e.getStatus()
         );
     }
+
+    // 데이터가 하나라도 비어있을때 발생
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        var errorMessage =
+                e.getFieldErrors().stream()
+                        .map(fieldError -> (fieldError.getField() + ": " + fieldError.getDefaultMessage()))
+                        .toList()
+                        .toString();
+
+        return new ResponseEntity<>(
+                new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage), HttpStatus.BAD_REQUEST);
+    }
+
+    // body값이 하나도 없을때 발생
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(HttpStatus.BAD_REQUEST, "Required request body is missing."),
+                HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler(RuntimeException.class) //RuntimeException 이발생할 때 여기서 가로챈다
     public ResponseEntity<ErrorResponse> handlerRuntimeException(RuntimeException e){
