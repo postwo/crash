f8e70c3d4eab7f58f164e64715a8393a1cf35da8
postwo <codo7717@naver.com>
2025-03-01 19:32:21 +0900

    SessionSpeaker dto 와 repository 생성

---
+++ src/main/java/com/example/crash/model/sessionspeaker/SessionSpeaker.java
@@ -0,0 +1,16 @@
+package com.example.crash.model.sessionspeaker;
+
+import com.example.crash.model.entity.SessionSpeakerEntity;
+
+public record SessionSpeaker(
+        Long speakerId, String company, String name, String description, String profile) {
+
+    public static SessionSpeaker from(SessionSpeakerEntity sessionSpeakerEntity) {
+        return new SessionSpeaker(
+                sessionSpeakerEntity.getSpeakerId(),
+                sessionSpeakerEntity.getCompany(),
+                sessionSpeakerEntity.getName(),
+                sessionSpeakerEntity.getDescription(),
+                sessionSpeakerEntity.getProfile());
+    }
+}
---
+++ src/main/java/com/example/crash/repository/SessionSpeakerEntityRepository.java
@@ -0,0 +1,8 @@
+package com.example.crash.repository;
+
+import com.example.crash.model.entity.SessionSpeakerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionSpeakerEntityRepository extends JpaRepository<SessionSpeakerEntity, Long> {
+
+}
